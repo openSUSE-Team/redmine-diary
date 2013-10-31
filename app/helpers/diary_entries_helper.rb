601f8ffe877a36b7283608e4aa4981338105a476
Ancor Gonzalez Sosa <ancor@suse.de>
2013-10-31 14:34:43 +0100

    New layout: one table per day with a header per user

--- app/helpers/diary_entries_helper.rb
+++ app/helpers/diary_entries_helper.rb
@@ -1,50 +1,47 @@
 module DiaryEntriesHelper
 
-  def close_table
-    if @table_is_open
-      @table_is_open = false
-      "</tbody>\n</table>\n".html_safe
+  def close_user
+    if @user_is_open
+      @user_is_open = false
+      "</tbody>\n".html_safe
     else
       ""
     end
   end
 
-  def open_table
-    @table_is_open = true
-    out = "<table class=\"list time-entries\">\n"
-    out += "<thead>\n<tr>\n"
-    @query.inline_columns.each do |column|
-      out += "<th>#{column.caption}</th>\n"
-    end
-    out += "<th></th>\n</tr>\n</thead>\n<tbody>\n"
-    out.html_safe
-  end
-
   def user_header(user)
     if user.id != @user_id
+      out = close_user.html_safe
       @user_id = user.id
-      out = close_table
-      out += content_tag(:h3, user.login)
-      out += open_table
-      out.html_safe
+      @user_is_open = true
+      out += "<thead>\n<tr>\n".html_safe
+      out += "<th class=\"diary-user\" colspan=\"#{@query.inline_columns.size+1}\">#{user.login}</th>\n".html_safe
+      out += "<tbody>\n".html_safe
+      raw out
     else
       ""
     end
   end
 
   def close_day
-    "</div>\n".html_safe
+    raw(close_user.html_safe + "</table>\n</div>\n".html_safe)
   end
 
   def open_day(day)
     if day != @day
-      out = close_table
-      out += close_day unless @day.nil?
+      out = @day.nil? ? "" : close_day.html_safe
+      @user_is_open = false
       @day = day
       @user_id = nil
       out += "<div class=\"diary-day\">\n".html_safe
       out += content_tag(:h2, day)
-      out.html_safe
+      out += "<table class=\"list time-entries\">\n".html_safe
+      out += "<thead>\n<tr>\n".html_safe
+      @query.inline_columns.each do |column|
+        out += "<th>#{column.caption}</th>\n".html_safe
+      end
+      out += "<th></th>\n</tr>\n</thead>\n".html_safe
+      raw out
     else
       ""
     end
